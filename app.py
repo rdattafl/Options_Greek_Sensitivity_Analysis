8242f3863974b7519ad643690277c31b14d9b497
Riju Datta <dattarij@seas.upenn.edu>
2025-04-11 13:30:15 -0500

    Initial project setup

---
+++ app.py
@@ -0,0 +1,3 @@
+# This web app is designed to allow users to explore how the Greeks (i.e., Delta, Gamma, Theta, Vega, and Rho) affect option pricing under the 
+# Black-Scholes-Merton (BSM) model by adjusting input parameters (ex: strike price, time to maturity, volatility, interest rate, etc.) using sliders.
+# The web app was designed using Streamlit and plotly.
